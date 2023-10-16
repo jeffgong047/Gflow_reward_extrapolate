f1bcb995f99d357e18f2d0a5f6817d9796663837
jeffgong047 <66967298+jeffgong047@users.noreply.github.com>
2023-10-16 17:17:40 -0400

    Update gflownet.py

--- src/gflownet.py
+++ src/gflownet.py
@@ -2,7 +2,7 @@ class Gflow_extrapolate():
     def __init__(self):
         pass
 
-    def backward_reward_propogation(self):
+    def backward_reward_propagation(self):
         pass
 
     def fit_edge_flow(self):
